1eece300fc33db56e4531aea6587bdae62d418c3
Lesto <1101z1111@gmail.com>
2013-11-16 14:41:06 +0600

    начало работы

---
+++ ClassLibrary/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary
+{
+    class Node
+    {
+        public string Name { get; set; }
+        public int Level { set; get; }
+        public List<Node> Children { set; get; }
+
+
+        public Node()
+        {
+            
+        }
+    }
+}
